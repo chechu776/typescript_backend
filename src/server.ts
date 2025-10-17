77916e14c2384ba28548b7bc443c70c989ca9e70
chechu776 <shameerchechu@gmail.com>
2025-10-17 10:04:22 +0530

    db connected

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,27 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
-import router from '../routes/routes';
+import router from '../routes/routes.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
-
 app.use('/api', router);
 
+const dbUrl = async ()=>{
+  try{
+    await mongoose.connect(process.env.DBURL || '');
+    console.log('Connected to the database'); 
+  }
+  catch(error){
+    console.error('Database connection error:', error);
+  } 
+}
+dbUrl();
+
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 export default app;
